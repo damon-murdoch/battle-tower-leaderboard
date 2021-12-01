2f0c94528dacfe33710c59f7b305570ea0cd9ebd
Damon Murdoch <Damon.Murdoch@qed.qld.gov.au>
2021-12-01 16:55:26 +1000

    Preparing leaderboard json file

---
+++ data/leaderboard.js
@@ -0,0 +1,145 @@
+const LEADERBOARD = {
+
+  // Row Contents: 
+  // 0: User Name
+  // 1: User Link (i.e. Twitter) (Optional)
+  // 2: User Record
+  // 3: Record Status (Finished/Ongoing)
+  // 4: User Team
+  // 5: User Proof
+  
+  gen3: [
+    {
+      name: "[Gen 3] [Ruby & Sapphire] Battle Tower", 
+      formats: {
+        "Singles": [
+
+        ], "Doubles": [
+
+        ]
+      }
+    }, 
+    {
+      name: "[Gen 3] [Emerald] Battle Tower", 
+      formats: {
+        "Singles": [
+
+        ], "Doubles": [
+
+        ]
+      }
+    },
+    {
+      name: "[Gen 3] [FireRed & LeafGreen] Trainer Tower", 
+      formats: {
+        "Singles": [
+
+        ], "Doubles": [
+
+        ]
+      }
+    }
+  ], 
+  gen4: [
+    {
+      name: "[Gen 4] [Diamond & Pearl] Battle Tower", 
+      formats: {
+        "Singles": [
+
+        ], "Doubles": [
+
+        ]
+      }
+    }, 
+    {
+      name: "[Gen 4] [Platinum] Battle Tower", 
+      formats: {
+        "Singles": [
+
+        ], "Doubles": [
+
+        ]
+      }
+    }, 
+    {
+      name: "[Gen 4] [Heart Gold & Soul Silver] Battle Tower", 
+      formats: {
+        "Singles": [
+
+        ], "Doubles": [
+
+        ]
+      }
+    }
+  ], 
+  gen5: [
+    {
+      name: "[Gen 5] [Black & White] Battle Subway", 
+      formats: {
+        "Singles": [
+          
+        ], "Doubles": [
+          ["Scrubbs", "https://twitter.com/SirScrubbington", 175, "Ongoing", "https://t.co/Iea8Wr7px4?amp=1", "https://twitter.com/SirScrubbington/status/1425625983449665536?s=20"]
+        ]
+      }
+    }, 
+    {
+      name: "[Gen 5] [Black 2 & White 2] Battle Subway", 
+      formats: {
+        "Singles": [
+          
+        ], "Doubles": [
+          ["Scrubbs", "https://twitter.com/SirScrubbington", 175, "Ongoing", "https://t.co/Iea8Wr7px4?amp=1", "https://twitter.com/SirScrubbington/status/1425625983449665536?s=20"]
+        ]
+      }
+    }
+  ],
+  gen6: [
+    {
+      name: "[Gen 6] [X & Y] Battle Miason",
+      formats: {
+        "Singles": [
+
+        ], "Doubles": [
+
+        ], "Triples": [
+
+        ]
+      }
+    },
+    {
+      name: "[Gen 6] [Omega Ruby & Alpha Sapphire] Battle Miason",
+      formats: {
+        "Singles": [
+
+        ], "Doubles": [
+
+        ], "Triples": [
+
+        ]
+      }
+    }
+  ], 
+  gen7: [
+    {
+      name: "[Gen 7] [Sun & Moon] Battle Tree", 
+      formats: {
+        "Singles": [
+
+        ], "Doubles": [
+
+        ]
+      }
+    }, 
+    {
+      name: "[Gen 7] [Ultra Sun & Ultra Moon] Battle Tree", 
+      formats: {
+        "Singles": [
+
+        ], "Doubles": [
+
+        ]
+      }
+    }
+  ]
+}
